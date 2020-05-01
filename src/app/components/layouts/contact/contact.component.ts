566935772d34b52d923d35dff172c5b423117bb5
V-OLOO <oloov534@gmail.com>
2020-05-01 11:16:31 +0300

    added to email

--- src/app/components/layouts/contact/contact.component.ts
+++ src/app/components/layouts/contact/contact.component.ts
@@ -38,6 +38,7 @@ export class ContactComponent implements OnInit {
           email: ['', [Validators.required, Validators.email]],
           subject: ['', [Validators.required]],
           message: ['', [Validators.required]],
+		  to_email: ['info@reinitresearch.com']
         });
     }
 
